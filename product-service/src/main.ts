9ce1daa648264a904b0d9f3dd28c008cb474918f
Tharindu-Sandeepa <katharindusandeepa@gmail.com>
2026-03-20 12:08:56 +0530

    Create main.ts

---
+++ product-service/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Product & Inventory Service')
+    .setDescription('The Product microservice API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3002);
+  console.log(`Product & Inventory Service running on port 3002`);
+}
+bootstrap();
