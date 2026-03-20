275a0163fda21fc9197617721922ecd8f7749bf5
Ridma Sathsara <158838958+Ridma-Sathsara@users.noreply.github.com>
2026-03-20 12:28:12 +0530

    Create main.ts

---
+++ notification-service/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notification & Audit Service')
+    .setDescription('The Notification microservice API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3004);
+  console.log(`Notification & Audit Service running on port 3004`);
+}
+bootstrap();
