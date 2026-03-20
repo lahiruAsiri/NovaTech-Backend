c5e05a13506791862662714e96242da0c4b4a769
Kasun Lakshitha <kasunlakshitha998@gmail.com>
2026-03-20 12:33:01 +0530

    app services

---
+++ admin-service/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Admin & User Service')
+    .setDescription('The Admin & User microservice API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3001);
+  console.log(`Admin & User Service running on port 3001`);
+}
+bootstrap();
