f5d8cc894ef3747c9cc25715e85e995fda0c60a9
lahiruasiri <lahiruasiri1@gmail.com>
2026-05-01 17:04:32 +0530

    Update main.ts

--- admin-service/src/main.ts
+++ admin-service/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Triggering Azure Admin Deploy
   const config = new DocumentBuilder()
     .setTitle('Admin & User Service')
     .setDescription('The Admin & User microservice API description')
