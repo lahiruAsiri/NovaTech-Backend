c63c5ef1764b67bdc75ce3cd92d7784172a5b2e0
lahiruasiri <lahiruasiri1@gmail.com>
2026-05-01 17:31:14 +0530

    Update main.ts

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Triggering Azure Admin Deploy1
   const config = new DocumentBuilder()
     .setTitle('Product & Inventory Service')
     .setDescription('The Product microservice API description')
